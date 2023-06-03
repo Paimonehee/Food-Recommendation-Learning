497d216036a0a64394309c62e6544419b31823e0
Paimonehee <135433760+Paimonehee@users.noreply.github.com>
2023-06-03 19:25:28 +0700

    Add files via upload

---
+++ templates/deploy.py
@@ -0,0 +1,30 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('food_pred_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    ordered =''
+    return render_template('index.html', output_prediction=ordered)
+
+
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+    favorite_bahan = float(request.form['favorite_bahan'])
+    gender = float(request.form['gender'])
+    umur = float(request.form['umur'])
+    alamat = float(request.form['alamat'])
+    rasa = float(request.form['rasa'])
+    occupation = float(request.form['occupation'])
+    fave_drink = float(request.form['fave_drink'])
+    menu_ordered = float(request.form['menu_ordered'])
+    result = model.predict([[favorite_bahan, gender, umur, alamat, rasa, occupation, fave_drink, menu_ordered]])[0]
+    return render_template('index.html', result=result)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
